cb6debb1f2fbcddf88a21670f46be7fa855562e1
Saurabh Gupta <saurabhgupta9369899665@gmail.com>
2023-05-06 23:49:30 +0530

    again

--- home_1/find_pairs_k_diff.cpp
+++ home_1/find_pairs_k_diff.cpp
@@ -1,14 +1,1 @@
-//1st approach
-  int findPairs(vector<int>& nums, int k) {
-        set<pair<int,int> >s;
-        sort(nums.begin(),nums.end());
-        for(int i = 0; i<size(nums);i++){
-            for(int j = i+1; j< size(nums); j++){
-                if(abs(nums[i]-nums[j])==k){
-                    s.insert({nums[i],nums[j]});
-                }
-            }
-        }
-        return s.size();
-    }
-    
+
