5ce3364178078d1d116a9f5a8600562983afdde3
Saurabh Gupta <saurabhgupta9369899665@gmail.com>
2023-05-27 23:57:00 +0530

    .

--- debug practice/first.cpp
+++ debug practice/first.cpp
@@ -46,7 +46,7 @@ int binarySearch(int arr[], int left, int right, int x) {
 }
 
 int main() {
-    int arr[] = { 2, 3, 4, 10, 40 };
+    int arr[] = { 2, 3, 4, 10, 40};
     int n = sizeof(arr) / sizeof(arr[0]);
     int x = 10;
     int result = binarySearch(arr, 0, n - 1, x);
